e378f22d843a09a013ea4df06948ce571e8f6651
RafaelLuckwu <rafaelluckwu2@gmail.com>
2026-01-22 20:46:05 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"log"
-	"microservice-order/config"
-	db "microservice-order/internal/adapters/db"
-	pay "microservice-order/internal/adapters/payment"
-	grpcServer "microservice-order/internal/adapters/grpc"
-	"microservice-order/internal/application/core/api"
-)
+import "log"
 
 func main() {
-	dbAdapter, _ := db.NewAdapter(config.GetDataSourceURL())
-	paymentAdapter, err := pay.NewAdapter(config.GetPaymentServiceUrl())
-	if err != nil { log.Fatalf("Payment error: %v", err) }
-
-	app := api.NewApplication(dbAdapter, paymentAdapter)
-	server := grpcServer.NewAdapter(app, config.GetApplicationPort())
-	server.Run()
+    log.Println("Payment service ativo")
 }
--- order.go
+++ order.go
@@ -1,26 +1,31 @@
-package domain
+package service
 
-import "time"
+import (
+    "context"
+    "log"
+    "time"
 
-type OrderItem struct {
-    ProductCode string `json:"product_code"`
-    UnitPrice   float32 `json:"unit_price"`
-    Quantity    int32 `json:"quantity"`
-}
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/codes"
+    "google.golang.org/grpc/status"
+)
 
-type Order struct {
-    ID         int64       `json:"id"`
-    CustomerID int64       `json:"customer_id"`
-    Status     string      `json:"status"`
-    OrderItems []OrderItem `json:"order_items"`
-    CreatedAt  int64       `json:"created_at"`
-}
+func CallPayment(conn *grpc.ClientConn) error {
+    ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+    defer cancel()
 
-func NewOrder(customerId int64, items []OrderItem) Order {
-    return Order{
-        CustomerID: customerId,
-        OrderItems: items,
-        Status: "Pending",
-        CreatedAt: time.Now().Unix(),
+    // Simulação de chamada gRPC
+    err := status.Error(codes.DeadlineExceeded, "timeout simulado")
+
+    if err != nil {
+        st, ok := status.FromError(err)
+        if ok && st.Code() == codes.DeadlineExceeded {
+            log.Println("Timeout ao chamar o serviço Payment (DeadlineExceeded)")
+        } else {
+            log.Printf("Erro ao chamar Payment: %v", err)
+        }
+        return err
     }
+
+    return nil
 }
